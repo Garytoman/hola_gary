9fe2fc5dca4c8a452a18997033827f6d3acbb999
Gary <garyto@hotmail.com>
2015-02-04 00:58:12 +0100

    rdoc implemented

--- lib/hola_gary.rb
+++ lib/hola_gary.rb
@@ -1,4 +1,14 @@
+# = hola_gary.rb
+#
+# Author::        Gary Márquez
+# e-mail::        garyjscs@gmail.com
+# Description::   Generates a string of 'Hello world' with translation posibilities
+#
+
 class HolaGary
+  # * without arguments: returns spanish translation of 'Hello world'
+  # * with ('english') argument: returns classic 'Hello world' string
+  # _NOTES:_ more languages in future versions will be available
   def self.hi(language = "spanish")
     translator = Translator.new(language)
     translator.hi
--- lib/hola_gary/translator.rb
+++ lib/hola_gary/translator.rb
@@ -1,8 +1,19 @@
+# = translator.rb
+#
+# Author::        Gary Márquez
+# e-mail::        garyjscs@gmail.com
+# Description::   Translates string of 'Hello world'
+#
+
 class Translator
+  # - Initialize the object and set language for translation
+  # - If language is not set returns the spanish version of string
   def initialize(language)
     @language = language
   end
   
+  # - Returns classic 'Hello world' string in selected language
+  # - If language is not set returns the spanish version of string
   def hi
     case @language
     when "english"
